44fc4400ce549c84440c903ee70696759bc3a31b
Lukas <odomabusiness@gmail.com>
2026-02-12 17:30:52 +0100

    Added non-prod banner at the top

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly IS_PRODUCTION: boolean;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
